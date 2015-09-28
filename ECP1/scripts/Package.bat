c200fb0f1f8504ecaf7c2ec9b85f4bba14ca8b10
Jorgelou <sirjorgete@gmail.com>
2015-09-28 22:36:06 +0200

    Jorge Rábanos Peña - BG0094

---
+++ ECP1/scripts/Package.bat
@@ -0,0 +1,18 @@
+@echo off
+
+set workspace=C:\Users\bg0094\Desktop\workspace\SPAI\ECP1\ECP1
+set PATH=%PATH%C:\Users\bg0094\Desktop\apache-maven-3.3.3-bin\apache-maven-3.3.3\bin
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_51
+set M2_HOME=C:\Users\bg0094\Desktop\apache-maven-3.3.3-bin\apache-maven-3.3.3
+
+echo -----------------------------------------
+
+echo .
+echo Workspace --- %workspace%
+echo .
+cd %workspace%
+echo ============ mvn clean test (profile: develop) =======================================================
+echo .
+call mvn clean test -Denvironment.type=develop
+call mvn clean package -Denvironment.type=preproduction
+pause
---
+++ ECP1/src/main/java/jorge/rabanos/ecp1/miw/upm/es/ECP1/App.java
@@ -0,0 +1,13 @@
+package jorge.rabanos.ecp1.miw.upm.es.ECP1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
