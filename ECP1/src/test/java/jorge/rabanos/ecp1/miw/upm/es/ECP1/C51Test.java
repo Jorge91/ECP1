bcefbbf83f877425902a770c484e06e1da776e71
Jorgelou <sirjorgete@gmail.com>
2015-09-28 22:59:32 +0200

    C51Test

---
+++ ECP1/src/test/java/jorge/rabanos/ecp1/miw/upm/es/ECP1/C51Test.java
@@ -0,0 +1,27 @@
+package jorge.rabanos.ecp1.miw.upm.es.ECP1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C51Test {
+
+private C51 c51;
+	
+	@Before
+	public void setUp() {
+		c51 = new C51();
+	}
+	
+	@Test
+	public void testM1() {
+		assertEquals("m1", c51.m1());
+	}
+	
+	@Test
+	public void testM2() {
+		assertEquals("m2", c51.m2());
+	}
+
+}
